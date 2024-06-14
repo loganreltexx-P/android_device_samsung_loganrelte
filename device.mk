c04aec3bbb2ec95b93e8cd6404104032c4763634
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2024-06-14 11:13:54 +0200

    loganrelte: attempt to convert serranolte tree

---
+++ device.mk
@@ -0,0 +1,33 @@
+# Copyright (C) 2013-2016, The CyanogenMod Project
+# Copyright (C) 2017, The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from loganrelte
+$(call inherit-product, $(COMMON_PATH)/loganrelte-common.mk)
+# Also get non-open-source specific aspects
+$(call inherit-product, $(DEVICE_VENDOR_PATH)/loganreltexx-vendor.mk)
+
+# Device overlay
+DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
+
+# FM radio
+PRODUCT_PACKAGES += \
+    android.hardware.broadcastradio@1.0-impl \
+    FM2 \
+    qcom.fmradio
---
+++ lineage_loganreltexx.mk
@@ -0,0 +1,35 @@
+# Copyright (C) 2013-2016, The CyanogenMod Project
+# Copyright (C) 2017, The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+COMMON_PATH        := device/samsung/loganrelte-common
+COMMON_VENDOR_PATH := vendor/samsung/loganrelte-common
+DEVICE_PATH        := device/samsung/loganreltexx
+DEVICE_VENDOR_PATH := vendor/samsung/loganreltexx
+PLATFORM_PATH      := device/samsung/msm8930-common
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from serranoltexx device
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_loganreltexx
+PRODUCT_DEVICE := loganreltexx
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MODEL := GT-S7275R
